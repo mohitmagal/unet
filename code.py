9ab894e8cac2b589cb8adbbeb46aa4ff9872bfc9
Mohit Mangal <mohit.mangal@wipro.com>
2018-03-09 16:26:11 +0530

    image size increased

--- code.py
+++ code.py
@@ -29,8 +29,8 @@ import tensorflow as tf
                       set some parameters
 ------------------------------------------------------------------------------
 '''
-IMG_WIDTH = 128
-IMG_HEIGHT = 128
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
 IMG_CHANNELS = 3
 TRAIN_PATH = '../stage1_train/'
 TEST_PATH = '../stage1_test/'
@@ -113,53 +113,55 @@ def mean_iou(y_true, y_pred):
 inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
 s = Lambda(lambda x: x / 255) (inputs)
 
-c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
+baseLayerSize = 64
+
+c1 = Conv2D(baseLayerSize, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
 c1 = Dropout(0.1) (c1)
-c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
+c1 = Conv2D(baseLayerSize, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
 p1 = MaxPooling2D((2, 2)) (c1)
 
-c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
+c2 = Conv2D(baseLayerSize*2, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
 c2 = Dropout(0.1) (c2)
-c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
+c2 = Conv2D(baseLayerSize*2, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
 p2 = MaxPooling2D((2, 2)) (c2)
 
-c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
+c3 = Conv2D(baseLayerSize*4, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
 c3 = Dropout(0.2) (c3)
-c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
+c3 = Conv2D(baseLayerSize*4, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
 p3 = MaxPooling2D((2, 2)) (c3)
 
-c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
+c4 = Conv2D(baseLayerSize*8, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
 c4 = Dropout(0.2) (c4)
-c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
+c4 = Conv2D(baseLayerSize*8, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
 p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
 
-c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
+c5 = Conv2D(baseLayerSize*16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
 c5 = Dropout(0.3) (c5)
-c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
+c5 = Conv2D(baseLayerSize*16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
 
-u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
+u6 = Conv2DTranspose(baseLayerSize*8, (2, 2), strides=(2, 2), padding='same') (c5)
 u6 = concatenate([u6, c4])
-c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
+c6 = Conv2D(baseLayerSize*8, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
 c6 = Dropout(0.2) (c6)
-c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
+c6 = Conv2D(baseLayerSize*8, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
 
-u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+u7 = Conv2DTranspose(baseLayerSize*4, (2, 2), strides=(2, 2), padding='same') (c6)
 u7 = concatenate([u7, c3])
-c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
+c7 = Conv2D(baseLayerSize*4, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
 c7 = Dropout(0.2) (c7)
-c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
+c7 = Conv2D(baseLayerSize*4, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
 
-u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
+u8 = Conv2DTranspose(baseLayerSize*2, (2, 2), strides=(2, 2), padding='same') (c7)
 u8 = concatenate([u8, c2])
-c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
+c8 = Conv2D(baseLayerSize*2, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
 c8 = Dropout(0.1) (c8)
-c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
+c8 = Conv2D(baseLayerSize*2, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
 
-u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
+u9 = Conv2DTranspose(baseLayerSize, (2, 2), strides=(2, 2), padding='same') (c8)
 u9 = concatenate([u9, c1], axis=3)
-c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
+c9 = Conv2D(baseLayerSize, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
 c9 = Dropout(0.1) (c9)
-c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
+c9 = Conv2D(baseLayerSize, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
 
 outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
 
@@ -177,7 +179,7 @@ model.summary()
 # Fit model
 earlystopper = EarlyStopping(patience=5, verbose=1)
 checkpointer = ModelCheckpoint('model-dsbowl2018-1.h5', verbose=1, save_best_only=True)
-results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=16, epochs=50, callbacks=[earlystopper, checkpointer])
+results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=64, epochs=50, callbacks=[earlystopper, checkpointer])
 
 
 '''
