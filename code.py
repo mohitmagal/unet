6badd4e36d633936b431ad9550a8903ee19819f5
Mohit Mangal <mohit.mangal@wipro.com>
2018-03-09 16:42:28 +0530

    batch size changes

--- code.py
+++ code.py
@@ -179,7 +179,7 @@ model.summary()
 # Fit model
 earlystopper = EarlyStopping(patience=5, verbose=1)
 checkpointer = ModelCheckpoint('model-dsbowl2018-1.h5', verbose=1, save_best_only=True)
-results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=64, epochs=50, callbacks=[earlystopper, checkpointer])
+results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=16, epochs=50, callbacks=[earlystopper, checkpointer])
 
 
 '''
